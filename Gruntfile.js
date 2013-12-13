8021b5680633a1afd51a80f2a4d3ee35ab2ff06a
Sven Lito <me@svenlito.com>
2013-12-13 11:50:49 +0000

    wip

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+/*global module:false*/
+
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    watch: {
+      files: 'app/index.js',
+      tasks: ['browserify']
+    },
+
+    browserify: {
+      build: {
+        options: {
+          debug: true,
+          shim: {
+            jquery: {
+              path: 'bower_components/jquery/jquery.js',
+              exports: '$'
+            },
+            underscore: {
+              path: 'bower_components/underscore/underscore.js',
+              exports: '_'
+            },
+            handlebars: {
+              path: 'bower_components/handlebars/handlebars.js',
+              exports: 'Handlebars'
+            },
+            backbone: {
+              path: 'bower_components/backbone/backbone.js',
+              exports: 'Backbone',
+              depends: {
+                underscore: 'underscore'
+              }
+            },
+            bootstrap: {
+              path: 'bower_components/bootstrap/dist/js/bootstrap.js',
+              exports: 'boostrap',
+              depends: {
+                jquery: '$'
+              }
+            },
+            gif: {
+              path: 'bower_components/gif.js/dist/gif.js',
+              exports: 'GIF'
+            }
+
+          }
+        },
+
+        src: ['index.js'],
+        dest: 'bundle.js'
+      }
+    }
+
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['browserify']);
+
+};
---
+++ app/index.js
@@ -0,0 +1,89 @@
+/*
+Generate GIF
+var gif = new GIF({ workers: 2, quality: 10, width: 640, height: 480 });
+gif.addFrame(ctx, {copy: true});
+gif.render();
+*/
+
+'use strict';
+
+require('gif');
+
+module.exports = (function() {
+
+  navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+  var constraints = { audio: false, video: true };
+  var video = document.querySelector('video');
+  var canvas = document.querySelector('canvas');
+  var button = document.querySelector('button');
+  var target = document.getElementById('target');
+  var ctx = canvas.getContext('2d');
+  var localMediaStream = null;
+
+
+  button.addEventListener('click', snapshot, false);
+  button.addEventListener('click', makeHug, false);
+
+
+  function successCallback(stream) {
+    if (global.URL) {
+      video.src = global.URL.createObjectURL(stream);
+      localMediaStream = stream;
+    } else {
+      video.src = stream;
+    }
+    video.play();
+  }
+
+  function pipeVideoToImage () {
+    setInterval(snapshot, 1000);
+  }
+
+  function errorCallback(error) {
+    console.log("navigator.getUserMedia error: ", error);
+  }
+
+  function makeHug () {
+
+    var gif = new GIF({
+      workers: 2,
+      quality: 1,
+      width: 160,
+      height: 120,
+    });
+
+    gif.on('finished', function(blob) {
+      target.src = global.URL.createObjectURL(blob);
+    });
+
+    var takePictures = setInterval (function () {
+      gif.addFrame(ctx, {copy: true, delay: 100});
+    }, 100 );
+
+    setTimeout(function() {
+      clearTimeout(takePictures);
+      gif.render();
+    }, 2000);
+
+  }
+
+  function snapshot () {
+    setInterval( function () {
+      if (localMediaStream) {
+        ctx.drawImage(video, 0, 0, 160, 120);
+        // "image/webp" works in Chrome.
+        // Other browsers will fall back to image/png.
+        document.querySelector('img').src = canvas.toDataURL('image/webp');
+      }
+    }, 100);
+  }
+
+  navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+
+}());
+
